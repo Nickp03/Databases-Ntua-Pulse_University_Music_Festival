4ca00c7763a781846dd9ae33e090533aea531a78
Vector <el21189@mail.ntua.gr>
2025-05-12 00:38:05 +0300

    Query 8

--- sql/Q08.sql
+++ sql/Q08.sql
@@ -1,1 +1,13 @@
 --Q08
+
+SET @date='2000-06-16'
+
+SELECT DISTINCT staff_id
+FROM staff_schedule
+WHERE staff_id NOT IN (
+    SELECT staff_id
+    FROM staff_schedule 
+    JOIN event 
+    ON event.event_id = staff_schedule.event_id
+    WHERE event.event_date = @date
+);
