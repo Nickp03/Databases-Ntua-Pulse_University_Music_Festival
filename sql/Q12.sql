05805997ccc08189eb3a61b29f1730fdb0723ecd
magdalinipliatsika <magdalini.pliatsika@gmail.com>
2025-05-12 15:46:42 +0300

    queries 12 13 14

--- sql/Q12.sql
+++ sql/Q12.sql
@@ -1,1 +1,11 @@
---Q12
+-- Q12
+SELECT 
+    e.event_date,
+    sr.role_name,
+    COUNT(ss.staff_id) AS total_staff
+FROM staff_schedule ss
+JOIN event e ON ss.event_id = e.event_id
+JOIN staff s ON s.staff_id = ss.staff_id
+JOIN staff_role sr ON sr.role_id = s.role_id
+GROUP BY e.event_date, sr.role_name
+ORDER BY e.event_date, sr.role_name;
--- sql/Q13.sql
+++ sql/Q13.sql
@@ -1,1 +1,13 @@
---Q13
+-- Q13
+SELECT 
+    a.artist_id,
+    a.stage_name,
+    COUNT(DISTINCT l.continent) AS continents_participated
+FROM performance p
+JOIN artist a ON p.artist_id = a.artist_id
+JOIN event e ON p.event_id = e.event_id
+JOIN festival f ON e.festival_id = f.festival_id
+JOIN location l ON f.location_id = l.location_id
+WHERE p.artist_id IS NOT NULL
+GROUP BY a.artist_id, a.stage_name
+HAVING COUNT(DISTINCT l.continent) >= 3;
