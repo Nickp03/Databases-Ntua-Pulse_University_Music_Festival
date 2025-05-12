a693c9b67af88aa5e29e7bb12ae8c2d11f05118d
magdalinipliatsika <magdalini.pliatsika@gmail.com>
2025-05-12 16:06:24 +0300

    query 15 + output

--- sql/Q15.sql
+++ sql/Q15.sql
@@ -1,1 +1,18 @@
 -- Q15
+SELECT 
+    o.first_name,
+    o.last_name,
+    a.stage_name AS artist_name,
+    SUM(
+        r.interpretation + r.sound_and_lighting + 
+        r.stage_presence + r.organization + r.overall_impression
+    ) AS total_score
+FROM review r
+JOIN ticket t ON r.ticket_id = t.ticket_id
+JOIN owner o ON t.owner_id = o.owner_id
+JOIN performance p ON r.performance_id = p.performance_id
+JOIN artist a ON a.artist_id = p.artist_id
+WHERE p.artist_id IS NOT NULL
+GROUP BY o.owner_id, a.artist_id
+ORDER BY total_score DESC
+LIMIT 5;
