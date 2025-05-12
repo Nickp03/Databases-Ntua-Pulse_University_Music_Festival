da5fc5d85291a71d9a21ae6f7366c215b08890ee
Vector <el21189@mail.ntua.gr>
2025-05-12 15:45:20 +0300

    Query 10 final

--- sql/Q10.sql
+++ sql/Q10.sql
@@ -1,1 +1,14 @@
 --Q10
+
+WITH Pairs AS (
+    SELECT artist_id, GROUP_CONCAT(genre_id ORDER BY genre_id) AS pairs
+    FROM artist_genre
+    GROUP BY artist_id
+    HAVING COUNT(*) > 1
+)
+SELECT pairs, COUNT(*) AS frequency
+FROM Pairs 
+GROUP BY pairs
+ORDER BY frequency DESC
+LIMIT 3;
+
