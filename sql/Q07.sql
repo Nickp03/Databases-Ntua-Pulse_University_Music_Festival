eb92b1767ea4312312cb556829dbc67655f5902f
magdalinipliatsika <magdalini.pliatsika@gmail.com>
2025-05-13 22:13:08 +0300

    small change in Q07

--- sql/Q07.sql
+++ sql/Q07.sql
@@ -1,13 +1,13 @@
 --Q07
-
-SELECT festival_id,SUM(staff.level_id)/COUNT(staff.level_id) AS Average_experience_level
-FROM event
-JOIN staff_schedule
-ON event.event_id=staff_schedule.event_id
-JOIN staff
-ON staff.staff_id=staff_schedule.staff_id
-WHERE staff_schedule.role_id=4
+SELECT 
+    festival_id,
+    SUM(level_id) / COUNT(level_id) AS Average_experience_level
+FROM event 
+JOIN staff_schedule ON event.event_id = staff_schedule.event_id
+JOIN staff ON staff.staff_id = staff_schedule.staff_id
+WHERE staff.role_id = 4 
 GROUP BY festival_id
 ORDER BY Average_experience_level ASC
 LIMIT 1;
+
 
