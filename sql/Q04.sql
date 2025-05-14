e19133850ca09b3f0b7e08d3e92943381ee2ebc7
Nickp03 <nickpantazop03@gmail.com>
2025-05-14 17:29:28 +0300

    change

--- sql/Q04.sql
+++ sql/Q04.sql
@@ -6,7 +6,7 @@ EXPLAIN FORMAT=JSON
 
 SELECT artist.stage_name, AVG(review_summary.avg_interpretation) AS avg_interpretation, AVG(review_summary.avg_overall_impression) AS avg_overall_impression
 FROM artist
-INNER JOIN performance FORCE INDEX (PRIMARY) ON performance.artist_id = artist.artist_id
+INNER JOIN performance ON performance.artist_id = artist.artist_id
 INNER JOIN review_summary ON review_summary.performance_id = performance.performance_id
 WHERE artist.artist_id = 58
 GROUP BY artist.artist_id, artist.stage_name;*/
