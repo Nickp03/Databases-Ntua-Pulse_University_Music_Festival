fb48a28fc263e947e4fa5289b054862dc1b618d7
Nickp03 <nickpantazop03@gmail.com>
2025-05-14 13:49:15 +0300

    Q05 done

--- sql/Q05.sql
+++ sql/Q05.sql
@@ -1,14 +1,12 @@
 -- Q05
 
 WITH participation_counts AS (
-	SELECT artist.artist_id AS artist_id, artist.stage_name AS stage_name, COUNT(DISTINCT event.festival_id) AS festival_participations
-	FROM artist
-	JOIN performance ON performance.artist_id = artist.artist_id
-	JOIN event ON performance.event_id = event.event_id
-	WHERE TIMESTAMPDIFF(YEAR, artist.DOB, CURRENT_DATE()) < 30
-GROUP BY artist.artist_id, artist.stage_name)
-SELECT participation_counts.artist_id, participation_counts.stage_name, participation_counts.festival_participations
-FROM participation_counts
-WHERE participation_counts.festival_participations = (
-	SELECT MAX(inner_counts.festival_participations)
-	FROM participation_counts AS inner_counts);
+	SELECT a.artist_id, a.stage_name, TIMESTAMPDIFF(YEAR, a.DOB, CURDATE()) AS age, COUNT(p.performance_id) AS performance_count
+	FROM artist AS a
+	JOIN performance AS p ON p.artist_id = a.artist_id
+	JOIN event AS e ON p.event_id = e.event_id
+	WHERE TIMESTAMPDIFF(YEAR, a.DOB, CURDATE()) < 30
+	GROUP BY a.artist_id, a.stage_name)
+SELECT pc.stage_name, pc.age, pc.performance_count
+FROM participation_counts AS pc
+WHERE pc.performance_count = (SELECT MAX(performance_count) FROM participation_counts);
