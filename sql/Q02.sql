817e6c337f3de844b64ef031d190f4022d53f291
Nickp03 <nickpantazop03@gmail.com>
2025-05-14 12:27:12 +0300

    fixed location and Q02 try

--- sql/Q02.sql
+++ sql/Q02.sql
@@ -1,13 +1,17 @@
 -- Q02
 
--- Ορισμός της μεταβλητής εισόδου για το έτος
-SET @input_year = 2023;
+-- Ορισμός των μεταβλητών εισόδου για το έτος και το είδος
+SET @input_year = 2021;
+SET @input_genre = 'rock';
 
-SELECT DISTINCT pulse_university.artist.stage_name, pulse_university.genre.genre_name AS genre
-FROM pulse_university.artist
-INNER JOIN pulse_university.genre ON pulse_university.artist.genre_id = pulse_university.genre.genre_id
-INNER JOIN pulse_university.performance ON pulse_university.performance.artist_id = pulse_university.artist.artist_id
-INNER JOIN pulse_university.event ON pulse_university.performance.event_id = pulse_university.event.event_id
-INNER JOIN pulse_university.festival ON pulse_university.event.festival_id = pulse_university.festival.festival_id
-WHERE pulse_university.festival.year = @input_year
-ORDER BY pulse_university.artist.stage_name;
+SELECT a.stage_name, g.genre_name AS genre, CASE WHEN f_in.year IS NOT NULL THEN 'Ναι' ELSE 'Όχι' END AS Συμμετείχε
+FROM artist AS a
+JOIN artist_genre AS ag ON a.artist_id = ag.artist_id
+JOIN genre AS g ON ag.genre_id = g.genre_id AND g.genre_name = @input_genre -- Κρατάμε καλλιτέχνες ίδιου είδους
+LEFT JOIN (
+    SELECT DISTINCT p.artist_id, f.year
+    FROM performance AS p
+    JOIN event AS e ON p.event_id = e.event_id
+    JOIN festival AS f ON e.festival_id = f.festival_id
+    WHERE f.year = @input_year) AS f_in ON a.artist_id = f_in.artist_id -- Προσωρινός πίνακας με όλους τους καλλιτέχνες που είχαν εμφάνιση στο φεστιβάλ του έτους
+ORDER BY a.stage_name;
