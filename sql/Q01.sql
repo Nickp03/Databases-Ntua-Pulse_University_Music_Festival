21a2d5bac26828262b0eb574eee60b3081ae2cb0
Nickp03 <nickpantazop03@gmail.com>
2025-05-14 11:45:07 +0300

    Fixed Q01 to the correct format

--- sql/Q01.sql
+++ sql/Q01.sql
@@ -1,8 +1,12 @@
 -- Q01
 
-SELECT festival.year AS festival_year, ticket.method_of_purchase AS payment_method, SUM(ticket.cost) AS total_revenue
-FROM festival
-JOIN event ON event.festival_id = festival.festival_id
-JOIN ticket ON ticket.event_id = event.event_id
-GROUP BY festival.year, ticket.method_of_purchase
-ORDER BY festival.year, ticket.method_of_purchase;
+SELECT f.year AS festival_year,
+    COALESCE(SUM(CASE WHEN t.method_of_purchase = 'bank_account' THEN t.cost END), 0) AS bank_account,
+    COALESCE(SUM(CASE WHEN t.method_of_purchase = 'credit' THEN t.cost END), 0) AS credit,
+    COALESCE(SUM(CASE WHEN t.method_of_purchase = 'debit' THEN t.cost END), 0) AS debit,
+    SUM(t.cost) AS total_revenue
+FROM festival AS f
+JOIN event AS e ON e.festival_id = f.festival_id
+JOIN ticket AS t ON t.event_id = e.event_id
+GROUP BY f.year
+ORDER BY f.year;
