b47cde43fa3408dede480bc6ccfa27fd933c495d
Nickp03 <nickpantazop03@gmail.com>
2025-05-14 22:48:05 +0300

    Q04

--- sql/Q04.sql
+++ sql/Q04.sql
@@ -8,7 +8,7 @@ SELECT artist.stage_name, AVG(review_summary.avg_interpretation) AS avg_interpre
 FROM artist
 INNER JOIN performance ON performance.artist_id = artist.artist_id
 INNER JOIN review_summary ON review_summary.performance_id = performance.performance_id
-WHERE artist.artist_id = 58
+WHERE artist.artist_id = 9
 GROUP BY artist.artist_id, artist.stage_name;
 
 -- Nested loop join with force index
@@ -17,7 +17,7 @@ EXPLAIN FORMAT=JSON
 
 SELECT artist.stage_name, AVG(review_summary.avg_interpretation) AS avg_interpretation, AVG(review_summary.avg_overall_impression) AS avg_overall_impression
 FROM artist
-INNER JOIN performance FORCE INDEX (PRIMARY) ON performance.artist_id = artist.artist_id
+INNER JOIN performance FORCE INDEX (artist_id) ON performance.artist_id = artist.artist_id
 INNER JOIN review_summary ON review_summary.performance_id = performance.performance_id
 WHERE artist.artist_id = 58
 GROUP BY artist.artist_id, artist.stage_name;*/
